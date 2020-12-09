f992d7cf435c9bf433cb9978db82c71977e1cbfd
krisfiction <krisfiction@gmail.com>
2020-12-08 22:45:39 -0500

    refactor monster health

--- Arena/Characters/Monster.cs
+++ Arena/Characters/Monster.cs
@@ -6,10 +6,19 @@ namespace Arena.Characters
 {
     public class Monster : Character
     {
+        public static Random random = new Random();
+
         // values for random health based on high/low - so monsters will have varying health
         public int HealthLow { get; set; }
         public int HealthHigh { get; set; }
 
+        public int SetHealth()
+        {
+            //Health = random.Next(HealthLow, HealthHigh);
+            //HealthMax = Health;
+
+            return random.Next(HealthLow, HealthHigh);
+        }
 
     }
 }
